51b70ca12ce762944d9870df5536a2db865bfdb4
Bert-cheng <Cyunlong0207@gmail.com>
2021-03-19 20:43:14 +0800

    Finish eliminate part

--- Sprint-3/src/Sprint3.java
+++ Sprint-3/src/Sprint3.java
@@ -1,39 +1,39 @@
 
 public class Sprint3 {
 
-	public static void main (String args[]) {	   
+	public static void main(String args[]) {
 		Board board = new Board();
 		UI ui = new UI(board);
 		Player[] players = new Player[GameData.NUM_PLAYERS_PLUS_NEUTRALS];
-		Player currPlayer;
+		Player currPlayer, winner;
 		Card card;
-		int playerId, countryId, numUnits, numCards;
+		int playerId, countryId, numUnits = 0, numCards;
 		String name;
-		
+
 		ui.displayString("ENTER PLAYER NAMES");
 		ui.displayMap();
-		for (playerId=0; playerId<GameData.NUM_PLAYERS_PLUS_NEUTRALS; playerId++) {
+		for (playerId = 0; playerId < GameData.NUM_PLAYERS_PLUS_NEUTRALS; playerId++) {
 			if (playerId < GameData.NUM_PLAYERS) {
 				name = ui.inputName(playerId);
 				numUnits = GameData.INIT_UNITS_PLAYER;
 			} else {
 				name = "Neutral " + (playerId - GameData.NUM_PLAYERS + 1);
-				ui.displayName(playerId,name);
+				ui.displayName(playerId, name);
 				numUnits = GameData.INIT_UNITS_NEUTRAL;
 			}
-			players[playerId] = new Player (playerId, name, numUnits);
+			players[playerId] = new Player(playerId, name, numUnits);
 		}
-		
+
 		ui.displayString("\nDRAW TERRITORY CARDS FOR STARTING COUNTRIES");
 		Deck deck = new Deck();
-		for (playerId=0; playerId<GameData.NUM_PLAYERS_PLUS_NEUTRALS; playerId++) {
+		for (playerId = 0; playerId < GameData.NUM_PLAYERS_PLUS_NEUTRALS; playerId++) {
 			currPlayer = players[playerId];
 			if (playerId < GameData.NUM_PLAYERS) {
 				numCards = GameData.INIT_COUNTRIES_PLAYER;
 			} else {
 				numCards = GameData.INIT_COUNTRIES_NEUTRAL;
 			}
-			for (int i=0; i<numCards; i++) {
+			for (int i = 0; i < numCards; i++) {
 				card = deck.getCard();
 				ui.displayCardDraw(currPlayer, card);
 				currPlayer.subtractUnits(1);
@@ -41,14 +41,14 @@ public class Sprint3 {
 			}
 		}
 		ui.displayMap();
-		
+
 		ui.displayString("\nROLL DICE TO SEE WHO REINFORCES THEIR COUNTRIES FIRST");
 		do {
-			for (int i=0; i<GameData.NUM_PLAYERS; i++) {
+			for (int i = 0; i < GameData.NUM_PLAYERS; i++) {
 				players[i].rollDice(1);
 				ui.displayDice(players[i]);
 			}
-		} while (players[0].getDie(0) == players[1].getDie(0)); 
+		} while (players[0].getDie(0) == players[1].getDie(0));
 		if (players[0].getDie(0) > players[1].getDie(0)) {
 			playerId = 0;
 		} else {
@@ -57,33 +57,25 @@ public class Sprint3 {
 		currPlayer = players[playerId];
 		ui.displayRollWinner(currPlayer);
 		/*
-		ui.displayString("\nREINFORCE INITIAL COUNTRIES");
-		while (currPlayer.getNumUnits() > 0) {
-			ui.inputPlacement(currPlayer, currPlayer);
-			countryId = ui.getCountryId();
-			currPlayer.subtractUnits(3);
-			board.addUnits(countryId, currPlayer, 3);
-			ui.displayMap();
-			for (int i=GameData.NUM_PLAYERS; i<GameData.NUM_PLAYERS_PLUS_NEUTRALS; i++) {
-				ui.inputPlacement(currPlayer, players[i]);
-				countryId = ui.getCountryId();
-				currPlayer.subtractUnits(1);
-				board.addUnits(countryId, currPlayer, 1);	
-				ui.displayMap();
-			}
-			playerId = (++playerId)%GameData.NUM_PLAYERS;
-			currPlayer = players[playerId];
-		}
-		*/
+		 * ui.displayString("\nREINFORCE INITIAL COUNTRIES"); while
+		 * (currPlayer.getNumUnits() > 0) { ui.inputPlacement(currPlayer, currPlayer);
+		 * countryId = ui.getCountryId(); currPlayer.subtractUnits(3);
+		 * board.addUnits(countryId, currPlayer, 3); ui.displayMap(); for (int
+		 * i=GameData.NUM_PLAYERS; i<GameData.NUM_PLAYERS_PLUS_NEUTRALS; i++) {
+		 * ui.inputPlacement(currPlayer, players[i]); countryId = ui.getCountryId();
+		 * currPlayer.subtractUnits(1); board.addUnits(countryId, currPlayer, 1);
+		 * ui.displayMap(); } playerId = (++playerId)%GameData.NUM_PLAYERS; currPlayer =
+		 * players[playerId]; }
+		 */
 		/* Sprint3 start here: */
-		//Roll dice first.
+		// Roll dice first.
 		ui.displayString("\nROLL DICE TO SEE WHO START FIRST");
 		do {
-			for (int i=0; i<GameData.NUM_PLAYERS; i++) {
+			for (int i = 0; i < GameData.NUM_PLAYERS; i++) {
 				players[i].rollDice(1);
 				ui.displayDice(players[i]);
 			}
-		} while (players[0].getDie(0) == players[1].getDie(0)); 
+		} while (players[0].getDie(0) == players[1].getDie(0));
 		if (players[0].getDie(0) > players[1].getDie(0)) {
 			playerId = 0;
 		} else {
@@ -91,70 +83,87 @@ public class Sprint3 {
 		}
 		currPlayer = players[playerId];
 		ui.displayRollWinner(currPlayer);
-		
-		//Reinforcement Phase
+
+		// Reinforcement Phase
 		ui.displayString("\n(P1)[REINFORCE COUNTRIES]");
 		while (currPlayer.getNumUnits() > 0) {
 			ui.inputPlacementP1(currPlayer, currPlayer);
 			countryId = ui.getCountryId();
-			
-			//Calculate number of reinforcements
+
+			// Calculate number of reinforcements
 			int reinforce_num = board.getNumOfCountry(playerId);
-			if(reinforce_num/3 < 3) {
-				reinforce_num = 3; 
-			}else {
-				reinforce_num = reinforce_num/3;
+			if (reinforce_num / 3 < 3) {
+				reinforce_num = 3;
+			} else {
+				reinforce_num = reinforce_num / 3;
 			}
 			int num = board.getOccupieContinent(playerId);
-			if(num != -1) {
+			if (num != -1) {
 				reinforce_num += num;
 				ui.displayString("[Occupied Continent: true]");
-			}else {
+			} else {
 				ui.displayString("[Occupied Continent: false]");
 			}
-			ui.displayString("[num  of reinforcement: "+reinforce_num+"]");
+			ui.displayString("[num  of reinforcement: " + reinforce_num + "]");
 			board.addUnits(countryId, currPlayer, reinforce_num);
 			ui.displayMap();
-	
+
 			/*
-			//place for neutrals
-			for (int i=GameData.NUM_PLAYERS; i<GameData.NUM_PLAYERS_PLUS_NEUTRALS; i++) {
-				ui.inputPlacement(currPlayer, players[i]);
-				countryId = ui.getCountryId();
-				currPlayer.subtractUnits(1);
-				board.addUnits(countryId, currPlayer, 1);	
-				ui.displayMap();
-			}
-			*/
+			 * //place for neutrals for (int i=GameData.NUM_PLAYERS;
+			 * i<GameData.NUM_PLAYERS_PLUS_NEUTRALS; i++) { ui.inputPlacement(currPlayer,
+			 * players[i]); countryId = ui.getCountryId(); currPlayer.subtractUnits(1);
+			 * board.addUnits(countryId, currPlayer, 1); ui.displayMap(); }
+			 */
 			boolean b = false;
-			//Combat phase
+			// Combat phase
 			String combatChoice = ui.inputCombatChoice();
-			while(!combatChoice.equals("skip")) {
-				if(b) {
+			while (!combatChoice.equals("skip")) {
+				if (b) {
 					ui.displayString("You do not have extra army to combat. Combat phase ends automatically.");
 					break;
 				}
-				board.combat(ui, currPlayer,players);
+				board.combat(ui, currPlayer, players);
 				ui.displayMap();
 				combatChoice = ui.inputCombatChoice();
 			}
-			
-			//Fortify phase
+
+			// Fortify phase
 			String fortifyChoice = ui.inputFortifyChoice();
-			if(!fortifyChoice.equals("skip")) {
-				if(b) {
+			if (!fortifyChoice.equals("skip")) {
+				if (b) {
 					ui.displayString("You do not have extra army to fortify. Fortify phase ends automatically.");
 					break;
 				}
 				board.fortify(ui, currPlayer);
 				ui.displayMap();
 			}
-			
-			//move on to next player.
-			playerId = (++playerId)%GameData.NUM_PLAYERS;
+
+			// move on to next player.
+			playerId = (++playerId) % GameData.NUM_PLAYERS;
 			currPlayer = players[playerId];
 		}
-		
+		// Deal with neutral player’s armies are eliminated
+		for (playerId = 2; playerId < GameData.NUM_PLAYERS_PLUS_NEUTRALS; playerId++) {
+			if (numUnits == 0) {
+				// move to next player.
+				playerId = (++playerId) % GameData.NUM_PLAYERS;
+				currPlayer = players[playerId];
+			}
+
+		}
+
+		// Deal with human player’s armies are eliminated
+		for (playerId = 0; playerId < GameData.NUM_PLAYERS; playerId++) {
+			if (numUnits == 0) {
+				if (playerId == 0)
+					playerId = 0;
+				else
+					playerId = 1;
+				winner = players[playerId];
+				ui.displayWinner(winner);
+			}
+		}
+
 		return;
 	}
 
--- Sprint-3/src/UI.java
+++ Sprint-3/src/UI.java
@@ -8,13 +8,13 @@ public class UI {
 	private static String PROMPT = "> ";
 
 	private JFrame frame = new JFrame();
-	private MapPanel mapPanel;	
+	private MapPanel mapPanel;
 	private InfoPanel infoPanel = new InfoPanel();
 	private CommandPanel commandPanel = new CommandPanel();
 	private Parse parse = new Parse();
 	private Board board;
-	
-	UI (Board inBoard) {
+
+	UI(Board inBoard) {
 		board = inBoard;
 		mapPanel = new MapPanel(board);
 		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
@@ -22,62 +22,62 @@ public class UI {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(mapPanel, BorderLayout.NORTH);
 		frame.add(infoPanel, BorderLayout.CENTER);
-		frame.add(commandPanel,BorderLayout.SOUTH);
+		frame.add(commandPanel, BorderLayout.SOUTH);
 		frame.setResizable(false);
 		frame.setVisible(true);
 		return;
 	}
-	
-	public String makeLongName (Player player) {
+
+	public String makeLongName(Player player) {
 		return player.getName() + " (" + mapPanel.getColorName(player.getId()) + ") ";
 	}
-	
-	public void displayMap () {
+
+	public void displayMap() {
 		mapPanel.refresh();
 		return;
 	}
 
-	public void displayString (String string) {
+	public void displayString(String string) {
 		infoPanel.addText(string);
 		return;
 	}
-	
-	public void displayName (int playerId, String name) {
-		displayString("Neutral player " + (playerId+1) + " is " + mapPanel.getColorName(playerId));
-		return;		
+
+	public void displayName(int playerId, String name) {
+		displayString("Neutral player " + (playerId + 1) + " is " + mapPanel.getColorName(playerId));
+		return;
 	}
 
-	public void displayCardDraw (Player player, Card card) {
+	public void displayCardDraw(Player player, Card card) {
 		displayString(makeLongName(player) + " draws the " + card.getCountryName() + " card");
 		return;
 	}
-	
-	public void displayDice (Player player) {
-		displayString(makeLongName(player) + " rolls " + player.getDice() );
+
+	public void displayDice(Player player) {
+		displayString(makeLongName(player) + " rolls " + player.getDice());
 		return;
 	}
-	
-	public void displayRollWinner (Player player) {
-		displayString(makeLongName(player)  + " wins roll and goes first");
+
+	public void displayRollWinner(Player player) {
+		displayString(makeLongName(player) + " wins roll and goes first");
 		return;
 	}
-	
-	public String inputName (int playerId) {
+
+	public String inputName(int playerId) {
 		String response;
-		displayString("Enter the name for player " + (playerId+1) + " (" + mapPanel.getColorName(playerId) + "):");
+		displayString("Enter the name for player " + (playerId + 1) + " (" + mapPanel.getColorName(playerId) + "):");
 		response = commandPanel.getCommand();
 		response.trim();
 		displayString(PROMPT + response);
-		return response;		
+		return response;
 	}
-		
-	public void inputPlacement (Player byPlayer, Player forPlayer) {
+
+	public void inputPlacement(Player byPlayer, Player forPlayer) {
 		String response, message;
 		boolean placementOK = false;
 		do {
 			message = makeLongName(byPlayer) + ": Enter a country to reinforce with ";
 			if (byPlayer.equals(forPlayer)) {
-				message += "your own units";				
+				message += "your own units";
 			} else {
 				message += makeLongName(forPlayer) + "'s units";
 			}
@@ -97,29 +97,29 @@ public class UI {
 		} while (!placementOK);
 		return;
 	}
-	
-	public int getCountryId () {
+
+	public int getCountryId() {
 		return parse.getCountryId();
 	}
-	
+
 	public void inputPlacementP1(Player byPlayer, Player forPlayer) {
 		String response, message;
 		boolean placementOK = false;
 		do {
-			//display messages
+			// display messages
 			message = makeLongName(byPlayer) + ": Enter a country to reinforce with ";
 			if (byPlayer.equals(forPlayer)) {
-				message += "your own units";				
+				message += "your own units";
 			} else {
 				message += makeLongName(forPlayer) + "'s units";
 			}
 			displayString(message);
-			
-			//get inputs & display
+
+			// get inputs & display
 			response = commandPanel.getCommand();
 			displayString(PROMPT + response);
-			
-			//handle error input.
+
+			// handle error input.
 			parse.countryId(response);
 			if (parse.isError()) {
 				displayString("Error: Not a country");
@@ -130,38 +130,40 @@ public class UI {
 					placementOK = true;
 				}
 			}
-			
+
 		} while (!placementOK);
 		return;
 	}
-	
+
 	public String inputCombatChoice() {
 		String response;
 		displayString("Enter skip to skip the combat phase, or anything else to combat.");
 		response = commandPanel.getCommand();
 		return response;
 	}
-	
+
 	public String inputFortifyChoice() {
 		String response;
 		displayString("Enter skip to skip the fortify phase, or anything else to fortify.");
 		response = commandPanel.getCommand();
 		return response;
 	}
-	
-	public int inputModifiedTerritory(Player player, int mode) {	//mode 0 means the country chosen should belong to the player
-																	//mode 1 means the country chosen does not belong to the player
+
+	public int inputModifiedTerritory(Player player, int mode) { // mode 0 means the country chosen should belong to the
+																	// player
+																	// mode 1 means the country chosen does not belong
+																	// to the player
 		String response;
 		response = commandPanel.getCommand();
 		parse.countryId(response);
-		if(mode == 1) {
-			while(board.checkOccupier(player, parse.getCountryId())) {
+		if (mode == 1) {
+			while (board.checkOccupier(player, parse.getCountryId())) {
 				displayString("Error: Cannot choose your territory. Please choose another one:");
 				response = commandPanel.getCommand();
 				parse.countryId(response);
 			}
-		}else {
-			while(!board.checkOccupier(player, parse.getCountryId())) {
+		} else {
+			while (!board.checkOccupier(player, parse.getCountryId())) {
 				displayString("Error: You must choose your own territory. Please choose another one:");
 				response = commandPanel.getCommand();
 				parse.countryId(response);
@@ -169,166 +171,174 @@ public class UI {
 		}
 		return parse.getCountryId();
 	}
-	
+
 	public int inputArmyNum(int limit) {
 		String response;
 		response = commandPanel.getCommand();
 		int armyNum;
-		
-		while(true) {
+
+		while (true) {
 			try {
-	        	armyNum = Integer.parseInt(response);
-	            break;
-	        }catch (NumberFormatException e) {
-	        	displayString("Error: Input is not a number. Please enter a number:");
-	        	response = commandPanel.getCommand();
-	        }
-	    }
-		
-		while(armyNum > limit || armyNum < 1) {
+				armyNum = Integer.parseInt(response);
+				break;
+			} catch (NumberFormatException e) {
+				displayString("Error: Input is not a number. Please enter a number:");
+				response = commandPanel.getCommand();
+			}
+		}
+
+		while (armyNum > limit || armyNum < 1) {
 			displayString("You must leave at least one army on your territory.");
 			displayString("Please enter a correct number:");
-			
+
 			response = commandPanel.getCommand();
-			
-			while(true) {
+
+			while (true) {
 				try {
-		        	armyNum = Integer.parseInt(response);
-		            break;
-		        }catch (NumberFormatException e) {
-		        	displayString("Error: Input is not a number. Please enter a number:");
-		        	response = commandPanel.getCommand();
-		        }
-		    }
+					armyNum = Integer.parseInt(response);
+					break;
+				} catch (NumberFormatException e) {
+					displayString("Error: Input is not a number. Please enter a number:");
+					response = commandPanel.getCommand();
+				}
+			}
 		}
-		
+
 		return armyNum;
 	}
-	
+
 	public int inputMovedArmyNumber(int least, int most) {
 		String response;
 		response = commandPanel.getCommand();
 		int armyNum;
-		
-		while(true) {
+
+		while (true) {
 			try {
-	        	armyNum = Integer.parseInt(response);
-	            break;
-	        }catch (NumberFormatException e) {
-	        	displayString("Error: Input is not a number. Please enter a number:");
-	        	response = commandPanel.getCommand();
-	        }
-	    }
-		
-		while(armyNum < least || armyNum >= most) {
-			displayString("You must move at least as many armies you used in the attack, and you need to leave at least one army on the current territory.");
-			displayString("Please enter a correct number:");
-			
-			response = commandPanel.getCommand();
-			
-			while(true) {
-				try {
-		        	armyNum = Integer.parseInt(response);
-		            break;
-		        }catch (NumberFormatException e) {
-		        	displayString("Error: Input is not a number. Please enter a number:");
-		        	response = commandPanel.getCommand();
-		        }
-		    }
+				armyNum = Integer.parseInt(response);
+				break;
+			} catch (NumberFormatException e) {
+				displayString("Error: Input is not a number. Please enter a number:");
+				response = commandPanel.getCommand();
+			}
 		}
-		
+
+		while (armyNum < least || armyNum >= most) {
+			displayString(
+					"You must move at least as many armies you used in the attack, and you need to leave at least one army on the current territory.");
+			displayString("Please enter a correct number:");
+
+			response = commandPanel.getCommand();
+
+			while (true) {
+				try {
+					armyNum = Integer.parseInt(response);
+					break;
+				} catch (NumberFormatException e) {
+					displayString("Error: Input is not a number. Please enter a number:");
+					response = commandPanel.getCommand();
+				}
+			}
+		}
+
 		return armyNum;
 	}
-	
+
 	public int[] attackAction(Player player) {
-		
+
 		displayString("Announce the territory you are attacking from:");
 		int attackingTerritory = inputModifiedTerritory(player, 0);
 		displayString("Announce the territory you are attacking:");
 		int attackedTerritory = inputModifiedTerritory(player, 1);
-		
+
 		int numUnits = board.getNumUnits(attackingTerritory);
-		while(numUnits <= 1) {
-			displayString("Error: You must attack from a country that has more than one unit on it. Reselect the attacking territory:");
+		while (numUnits <= 1) {
+			displayString(
+					"Error: You must attack from a country that has more than one unit on it. Reselect the attacking territory:");
 			displayString("Announce the territory you are attacking from:");
 			attackingTerritory = inputModifiedTerritory(player, 0);
 		}
-		
+
 		int[] adjacent = GameData.ADJACENT[attackingTerritory];
 		boolean adjacentBool = false;
-		
-		for(int i = 0; i < adjacent.length; i++) {
-			if(attackedTerritory == adjacent[i]) {
+
+		for (int i = 0; i < adjacent.length; i++) {
+			if (attackedTerritory == adjacent[i]) {
 				adjacentBool = true;
 				break;
 			}
 		}
-		
-		while(adjacentBool == false) {
+
+		while (adjacentBool == false) {
 			displayString("Error: Cannot attack territories that are not adjacent. "
-							+ "Please reselect attacking territory and attacked territory:");
-			
+					+ "Please reselect attacking territory and attacked territory:");
+
 			displayString("Announce the territory you are attacking:");
 			attackedTerritory = inputModifiedTerritory(player, 1);
 			displayString("Announce the territory you are attacking from:");
 			attackingTerritory = inputModifiedTerritory(player, 0);
-			
-			for(int i = 0; i < adjacent.length; i++) {
-				if(attackedTerritory == adjacent[i]) {
+
+			for (int i = 0; i < adjacent.length; i++) {
+				if (attackedTerritory == adjacent[i]) {
 					adjacentBool = true;
 					break;
 				}
 			}
 		}
-		
-		int[] attack = {attackedTerritory, attackingTerritory};
+
+		int[] attack = { attackedTerritory, attackingTerritory };
 		return attack;
 	}
-	
+
 	public int[] fortifyAction(Player player) {
 		displayString("Tip: Unlike combat phase, you can only do fortify once.");
-		
+
 		displayString("Announce the territory you are fortifying from:");
 		int from = inputModifiedTerritory(player, 0);
 		displayString("Announce the territory you are fortifying to:");
 		int to = inputModifiedTerritory(player, 0);
-		
-		
+
 		int numUnits = board.getNumUnits(from);
-		while(numUnits <= 1) {
-			displayString("Error: You must fortify from a country that has more than one unit on it. Reselect the territory:");
+		while (numUnits <= 1) {
+			displayString(
+					"Error: You must fortify from a country that has more than one unit on it. Reselect the territory:");
 			displayString("Announce the territory you are fortifying from:");
 			from = inputModifiedTerritory(player, 0);
 		}
-		
+
 		int[] adjacent = GameData.ADJACENT[from];
 		boolean adjacentBool = false;
-		
-		for(int i = 0; i < adjacent.length; i++) {
-			if(to == adjacent[i]) {
+
+		for (int i = 0; i < adjacent.length; i++) {
+			if (to == adjacent[i]) {
 				adjacentBool = true;
 				break;
 			}
 		}
-		
-		while(adjacentBool == false) {
+
+		while (adjacentBool == false) {
 			displayString("Error: Cannot fortify territories that are not adjacent. "
-							+ "Please reselect fortifyied territory and fortifing territory:");
-			
+					+ "Please reselect fortifyied territory and fortifing territory:");
+
 			displayString("Announce the territory you are fortifying to:");
 			to = inputModifiedTerritory(player, 0);
 			displayString("Announce the territory you are fortifying from:");
 			from = inputModifiedTerritory(player, 0);
-			
-			for(int i = 0; i < adjacent.length; i++) {
-				if(to == adjacent[i]) {
+
+			for (int i = 0; i < adjacent.length; i++) {
+				if (to == adjacent[i]) {
 					adjacentBool = true;
 					break;
 				}
 			}
 		}
-		
-		int[] fortify = {to, from};
+
+		int[] fortify = { to, from };
 		return fortify;
+	}
+
+	public void displayWinner(Player player) {
+		displayString(makeLongName(player) + " wins the game.");
+		displayString("GAME OVER!");
+		return;
 	}
 }
